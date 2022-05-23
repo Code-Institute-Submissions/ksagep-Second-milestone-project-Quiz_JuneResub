1d1259a20bd59c3e59293ba37b30a6c301d599cc
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-23 14:30:34 +0000

    Add missing semicolons to the end of rows.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-const startButton = document.getElementById('start-button')
-const nextButton = document.getElementById('next-button')
-const questionContainerElement = document.getElementById('question-container')
-let shuffledQuestions, currentQuestionIndex
-const questionElement = document.getElementById('question')
-const optionButtonElement = document.getElementById('option-button')
+const startButton = document.getElementById('start-button');
+const nextButton = document.getElementById('next-button');
+const questionContainerElement = document.getElementById('question-container');
+let shuffledQuestions, currentQuestionIndex;
+const questionElement = document.getElementById('question');
+const optionButtonElement = document.getElementById('option-button');
 
-startButton.addEventListener('click', startQuiz)
+startButton.addEventListener('click', startQuiz);
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
+  currentQuestionIndex++;
+  setNextQuestion();
 })
 function startQuiz() {
   startButton.classList.add('hide')
@@ -77,7 +77,6 @@ function clearStatusClass() {
 
 const questions = [
   {
-    id: 0,
     question: "Where does coffee come from?",
     answer:[
       { text: "Ethiopia", Correct: true },
@@ -87,7 +86,6 @@ const questions = [
     ]        
   },
   {
-    id: 1,
     question: "Which is not a roasting method?",
     answer:[{ text: "Spanish", isCorrect: false },
              { text: "Italian", isCorrect: false },
@@ -95,7 +93,6 @@ const questions = [
              { text: "British", isCorrect: true }]        
   },
   {
-    id: 2,
     question: "What color is the ripe coffee berry?",
     answer:[{ text: "Yellow", isCorrect: false },
              { text: "Green", isCorrect: false },
@@ -103,7 +100,6 @@ const questions = [
              { text: "Blue", isCorrect: false }]        
   },
   {
-    id: 3,
     question: "There is really no caffeine in decaffeinated coffee?",
     answer:[{ text: "Yes, it is true", isCorrect: false },
              { text: "Decaffeinated coffee contains 2-6% caffeine", isCorrect: true },
@@ -111,7 +107,6 @@ const questions = [
              { text: "What does decaffeinated coffee mean?", isCorrect: false }]        
   },
   {
-    id: 4,
     question: "Where they produce the most coffee in the world?",
     answer:[{ text: "Vietnam", isCorrect: false },
              { text: "Brasil", isCorrect: true },
@@ -119,7 +114,6 @@ const questions = [
              { text: "India", isCorrect: false }]        
   },
   {
-    id: 5,
     question: "What is not the main characteristic of robusta coffees?",
     answer:[{ text: "acidic", isCorrect: false },
              { text: "high in caffeine", isCorrect: false },
@@ -127,7 +121,6 @@ const questions = [
              { text: "full-bodied", isCorrect: false }]
   },
   {
-    id: 6,
     question: "What is not the main characteristic of arabica coffees?",
     answer:[{ text: "sweeter than the robusta", isCorrect: false },
              { text: "low in caffeine", isCorrect: false },
@@ -135,7 +128,6 @@ const questions = [
              { text: "smaller beans", isCorrect: true }]
   },
   {
-    id: 7,
     question: "When coffee was discovered?",
     answer:[{ text: "15th century", isCorrect: true },
              { text: "16th century", isCorrect: false },
@@ -143,7 +135,6 @@ const questions = [
              { text: "19th century", isCorrect: false }]
   },
   {
-    id: 8,
     question: "What percentage of the coffee stays warm if we add cream to it?",
     answer:[{ text: "10%", isCorrect: false },
              { text: "20%", isCorrect: true },
@@ -151,7 +142,6 @@ const questions = [
              { text: "if you have a microwave oven, always", isCorrect: false }]
   },
   {
-    id: 9,
     question: "In which country do people drink the most coffee?",
     answer:[{ text: "USA", isCorrect: false },
              { text: "Finland", isCorrect: true },
@@ -159,92 +149,4 @@ const questions = [
              { text: "Vietnam", isCorrect: false }]
   }
   ];
-  /** Set start **/
-  var start = true;
-  /** Iterate **/
-  function iterate(id) {
-    var result = document.getElementsByClassName("result");
-    result[0].innerText = "";
-    const question = document.getElementById("question");
-    question.innerText = questions[id].question;
-    /** Get the options and provide option text**/
-    const op1 = document.getElementById('op1');
-    const op2 = document.getElementById('op2');
-    const op3 = document.getElementById('op3');
-    const op4 = document.getElementById('op4');
-    op1.innerText = questions[id].answer[0].text;
-    op2.innerText = questions[id].answer[1].text;
-    op3.innerText = questions[id].answer[2].text;
-    op4.innerText = questions[id].answer[3].text;
-    /** The false and true value to the options **/
-    op1.value = questions[id].answer[0].isCorrect;
-    op2.value = questions[id].answer[1].isCorrect;
-    op3.value = questions[id].answer[2].isCorrect;
-    op4.value = questions[id].answer[3].isCorrect;
-    var selected = "";
-    /** Selections for ops **/
-    op1.addEventListener(
-          "click", () => {
-          op1.style.backgroundColor = "blue";
-          op2.style.backgroundColor = "floralwhite";
-          op3.style.backgroundColor = "floralwhite";
-          op4.style.backgroundColor = "floralwhite";
-          selected = op1.value;
-      }
-    );
-    op2.addEventListener(
-          "click", () => {
-          op1.style.backgroundColor = "floralwhite";
-          op2.style.backgroundColor = "blue";
-          op3.style.backgroundColor = "floralwhite";
-          op4.style.backgroundColor = "floralwhite";
-          selected = op2.value;
-      }
-    );
-    op3.addEventListener(
-          "click", () => {
-          op1.style.backgroundColor = "floralwhite";
-          op2.style.backgroundColor = "floralwhite";
-          op3.style.backgroundColor = "blue";
-          op4.style.backgroundColor = "floralwhite";
-          selected = op3.value;
-      }
-    );
-    op4.addEventListener(
-          "click", () => {
-          op1.style.backgroundColor = "floralwhite";
-          op2.style.backgroundColor = "floralwhite";
-          op3.style.backgroundColor = "floralwhite";
-          op4.style.backgroundColor = "blue";
-          selected = op4.value;
-      }
-    );
-    /** The evaluation buton **/
-    const evaluate = document.getElementsByClassName("evaluate");
-    /** Evaluate method **/
-    evaluate[0].addEventListener(
-      "click", () => {
-          if (selected == "true") {
-              result[0].innerHTML = "True";
-              result[0].style.color = "green";
-          } else {
-              result[0].innerHTML = "False";
-              result[0].style.color = "red";
-          }
-      }
-    );
-  }
-  if (start) {
-    iterate(0);
-  }
-  /** Next button and its method **/
-  const next = document.getElementsByClassName('next') [0];
-  var id = 0;
-  next.addEventListener("click", () => {
-    start = false;
-    if (id <= 9) {
-        id++;
-        iterate(id);
-        console.log(id);
-    }
-  });
+  
