3e9ff376a806d4a43a89d7885c0d6d28c3d9a47e
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-23 19:17:06 +0000

    Add semicolons to the end of rest rows.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,70 +9,70 @@ startButton.addEventListener('click', startQuiz);
 nextButton.addEventListener('click', () => {
   currentQuestionIndex++;
   setNextQuestion();
-})
+});
 function startQuiz() {
-  startButton.classList.add('hide')
-  questionContainerElement.classList.remove('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
+  startButton.classList.add('hide');
+  questionContainerElement.classList.remove('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
 }
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
+  questionElement.innerText = question.question;
   question.answer.array.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct); {
       button.dataset.correct = answer.correct
     }
-    button.addEventListener('click')
-    answerButtonsElement.appendChild(button)
+    button.addEventListener('click');
+    answerButtonsElement.appendChild(button);
   });
 }
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList('hide')
+  clearStatusClass(document.body);
+  nextButton.classList('hide');
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
 function optionSelection(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
   }
   
 }
 
 function setStatusClass(element) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
+  clearStatusClass(element);
+  if ('correct') {
+    element.classList.add('correct');
   } else {
-    element.classList.add('wrong')
+    element.classList.add('wrong');
   }
 }
 
 function clearStatusClass() {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
 
 const questions = [
